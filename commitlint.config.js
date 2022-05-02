a447100ac9059926dc7080c7ddc14d4a76e7950c
jerry <944673842@qq.com>
2022-05-03 00:27:12 +0800

    docs: 修改readme

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-"module.exports = {extends: ['@commitlint/config-conventional']}" 
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'style', 'refactor', 'perf', 'test', 'build', 'chore', 'revert']],
+        'subject-max-length': [1, 'always', 150], // subject建议150字以内
+    },
+};
